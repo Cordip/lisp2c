cd5f2c945564ca576d7c319c5c418c0d953586e8
r.badyrtenov <r.badyrtenov@tbank.ru>
2026-03-28 22:22:15 +0700

    code refactor

--- src/main/scala/lisp/emit/CodeGen.scala
+++ src/main/scala/lisp/emit/CodeGen.scala
@@ -1,8 +1,8 @@
 package lisp.emit
 
-import lisp.types.{CExpr, Statement}
 import lisp.types.CExpr.{CCall, CNumber, CVar}
 import lisp.types.Statement.*
+import lisp.types.{CExpr, Statement}
 
 object CodeGen:
 
@@ -12,13 +12,13 @@ object CodeGen:
   private def emitStatement(stmt: Statement): String =
     stmt match
       case Value(name, call) => s"LispVal* $name = ${emitCall(call)};"
-      case Return(expr)      => s"return ${emitExpr(expr)};"
+      case Return(expr) => s"return ${emitExpr(expr)};"
 
   private def emitCall(call: CCall): String =
     call.name + "(" + call.args.map(emitExpr).mkString(", ") + ")"
 
   private def emitExpr(expr: CExpr): String =
     expr match
-      case CNumber(value)    => value.toString
-      case CVar(name)        => name
+      case CNumber(value) => value.toString
+      case CVar(name) => name
       case CCall(name, args) => name + "(" + args.map(emitExpr).mkString(", ") + ")"
