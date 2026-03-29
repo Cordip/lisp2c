520fad41c1f0fa03b60ac730fbcb4f2bfeb80617
Ramil Badyrtenov <150883678+ramen-badr@users.noreply.github.com>
2026-03-29 21:53:39 +0700

    Revert "Normalize NIL symbol handling"

--- src/main/scala/lisp/transform/Transform.scala
+++ src/main/scala/lisp/transform/Transform.scala
@@ -11,12 +11,12 @@ object Transform:
 
   private def transform(input: SExpr): LispExpr =
     input match
-      case SNil                                            => LispNil
-      case SNumber(x)                                      => LispNumber(x)
-      case SBool(v)                                        => LispBool(v)
-      case SSymbol(value) if value.equalsIgnoreCase("nil") => LispNil
-      case SSymbol(value)                                  => LispSymbol(value)
-      case SList(Nil)                                      => LispNil
+      case SNil           => LispNil
+      case SNumber(x)     => LispNumber(x)
+      case SBool(v)       => LispBool(v)
+      case SSymbol("nil") => LispNil
+      case SSymbol(value) => LispSymbol(value)
+      case SList(Nil)     => LispNil
       case SList(SSymbol("if") :: cond :: thenBranch :: elseBranch :: Nil) => LispIf(transform(cond), transform(thenBranch), transform(elseBranch))
       case SList(SSymbol("if") :: _) => throw new Exception("if requires exactly 3 arguments: condition, then, else")
       case SList(SSymbol("quote") :: body :: Nil) => LispQuote(transformQuoted(body))
