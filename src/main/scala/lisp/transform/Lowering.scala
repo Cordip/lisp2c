da51d1e15644347d9b124fc9d3614fc4859f4bc4
r.badyrtenov <r.badyrtenov@tbank.ru>
2026-03-29 18:49:42 +0700

    full

--- src/main/scala/lisp/transform/Lowering.scala
+++ src/main/scala/lisp/transform/Lowering.scala
@@ -8,33 +8,36 @@ import lisp.types.{CExpr, LispExpr}
 object Lowering:
 
   def apply(input: LispExpr): CExpr =
+    lower(input)
+
+  private def lower(input: LispExpr): CExpr =
     input match
       case LispNil                              => CVar(lispNil)
-      case LispCons(car, cdr)                   => CCall(makeCons, List(apply(car), apply(cdr)))
+      case LispCons(car, cdr)                   => CCall(makeCons, List(lower(car), lower(cdr)))
       case LispNumber(value)                    => CCall(makeInt, List(CNumber(value)))
       case LispBool(true)                       => CVar(lispTrue)
       case LispBool(false)                      => CVar(lispFalse)
-      case LispSymbol(value)                    => throw new Exception(s"Unexpected symbol in lowering: $value")
+      case LispSymbol(value)                    => CCall(makeSymbol, List(CStringLit(value)))
       case LispQuote(body)                      => lowerQuote(body)
-      case LispIf(cond, thenBranch, elseBranch) => CIf(apply(cond), apply(thenBranch), apply(elseBranch))
+      case LispIf(cond, thenBranch, elseBranch) => CIf(lower(cond), lower(thenBranch), lower(elseBranch))
       case LispApply(LispSymbol(symbol), args)  => lowerFunc(symbol, args)
       case LispApply(function, _)               => throw new Exception(s"${function.show} is not callable")
 
-  private def lowerFunc(symbol: String, args: List[LispExpr]): CExpr =
-    functions.get(symbol) match
-      case Some((func, arity)) if args.length == arity => CCall(func, args.map(apply))
-      case Some(_)                                     => throw new Exception(s"Wrong arity for $symbol")
-      case None => throw new Exception(s"Unsupported function $symbol with ${args.length} args")
-
   private def lowerQuote(input: LispExpr): CExpr =
     input match
-      case LispNil          => CVar(lispNil)
-      case LispNumber(n)    => CCall(makeInt, List(CNumber(n)))
-      case LispBool(true)   => CVar(lispTrue)
-      case LispBool(false)  => CVar(lispFalse)
-      case LispCons(a, b)   => CCall(makeCons, List(lowerQuote(a), lowerQuote(b)))
+      case LispNil => CVar(lispNil)
+      case LispNumber(n) => CCall(makeInt, List(CNumber(n)))
+      case LispBool(true) => CVar(lispTrue)
+      case LispBool(false) => CVar(lispFalse)
+      case LispCons(a, b) => CCall(makeCons, List(lowerQuote(a), lowerQuote(b)))
       case LispSymbol(name) => CCall(makeSymbol, List(CStringLit(name)))
       case _ => throw new Exception(s"lowerQuote: unsupported expression: $input")
+
+  private def lowerFunc(symbol: String, args: List[LispExpr]): CExpr =
+    functions.get(symbol) match
+      case Some((func, arity)) if args.length == arity => CCall(func, args.map(lower))
+      case Some(_)                                     => throw new Exception(s"Wrong arity for $symbol")
+      case None => throw new Exception(s"Unsupported function $symbol with ${args.length} args")
 
   private val functions = Map(
     "cons" -> (makeCons, 2),
