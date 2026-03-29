367a93381fd5f35f0a34e534ce97847afade965f
r.badyrtenov <r.badyrtenov@tbank.ru>
2026-03-29 18:35:46 +0700

    рефакторинг

--- src/main/scala/lisp/transform/Transform.scala
+++ src/main/scala/lisp/transform/Transform.scala
@@ -11,28 +11,23 @@ object Transform:
 
   def transform(input: SExpr): LispExpr =
     input match
-      case SNil          => LispNil
-      case SNumber(x)    => LispNumber(x)
-      case SBool(v)      => LispBool(v)
+      case SNil           => LispNil
+      case SNumber(x)     => LispNumber(x)
+      case SBool(v)       => LispBool(v)
       case SSymbol("nil") => LispNil
-      case SSymbol(name) => LispSymbol(name)
-      case SList(Nil)    => LispNil
-      case SList(SSymbol("quote") :: body :: Nil) =>
-        LispQuote(transformQuoted(body))
-      case SList(SSymbol("quote") :: _) =>
-        throw new Exception("quote requires exactly 1 argument")
-      case SList(SSymbol("if") :: cond :: thenBranch :: elseBranch :: Nil) =>
-        LispIf(transform(cond), transform(thenBranch), transform(elseBranch))
-      case SList(SSymbol("if") :: _) =>
-        throw new Exception("if requires exactly 3 arguments: condition, then, else")
-      case SList(head :: args) =>
-        LispApply(transform(head), args.map(transform))
+      case SSymbol(value) => LispSymbol(value)
+      case SList(Nil)     => LispNil
+      case SList(SSymbol("quote") :: body :: Nil) => LispQuote(transformQuoted(body))
+      case SList(SSymbol("quote") :: _) => throw new Exception("quote requires exactly 1 argument")
+      case SList(SSymbol("if") :: cond :: thenBranch :: elseBranch :: Nil) => LispIf(transform(cond), transform(thenBranch), transform(elseBranch))
+      case SList(SSymbol("if") :: _) => throw new Exception("if requires exactly 3 arguments: condition, then, else")
+      case SList(head :: args) => LispApply(transform(head), args.map(transform))
 
   private def transformQuoted(input: SExpr): LispExpr =
     input match
-      case SNil              => LispNil
-      case SNumber(x)        => LispNumber(x)
-      case SBool(value)      => LispBool(value)
-      case SSymbol(name)     => LispSymbol(name)
-      case SList(Nil)        => LispNil
-      case SList(h :: rest)  => LispCons(transformQuoted(h), transformQuoted(SList(rest)))
+      case SNil             => LispNil
+      case SNumber(x)       => LispNumber(x)
+      case SBool(value)     => LispBool(value)
+      case SSymbol(name)    => LispSymbol(name)
+      case SList(Nil)       => LispNil
+      case SList(h :: rest) => LispCons(transformQuoted(h), transformQuoted(SList(rest)))
