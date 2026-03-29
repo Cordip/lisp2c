ae931e4eb7e2c0d4a69e4400d79e0dc7d4a09e7d
r.badyrtenov <r.badyrtenov@tbank.ru>
2026-03-29 20:07:11 +0700

    full

--- src/test/scala/lisp/transform/TransformTest.scala
+++ src/test/scala/lisp/transform/TransformTest.scala
@@ -43,10 +43,14 @@ class TransformTest extends munit.FunSuite:
     )
 
   test("if missing else throws"):
-    intercept[Exception] { Transform(SList(List(SSymbol("if"), SBool(true), SNumber(1)))) }
+    intercept[Exception] {
+      Transform(SList(List(SSymbol("if"), SBool(true), SNumber(1))))
+    }
 
   test("if extra args throws"):
-    intercept[Exception] { Transform(SList(List(SSymbol("if"), SBool(true), SNumber(1), SNumber(2), SNumber(3)))) }
+    intercept[Exception] {
+      Transform(SList(List(SSymbol("if"), SBool(true), SNumber(1), SNumber(2), SNumber(3))))
+    }
 
   test("if"):
     val input = SList(List(SSymbol("if"), SBool(true), SNumber(1), SNumber(2)))
